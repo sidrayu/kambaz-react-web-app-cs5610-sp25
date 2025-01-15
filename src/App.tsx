48979fb20f1deee38778e6c566337a70ae048698
sidrayu <sidra@udel.edu>
2025-01-14 16:51:14 -0800

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <p>WELCOME to web</p>
-    </>
-  )
+  return <h1>WELCOME to web</h1>
 }
 
 export default App
