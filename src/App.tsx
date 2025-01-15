9e53ad75c741b2a5dc7b3b4dd14e80178f8e5a61
sidrayu <sidra@udel.edu>
2025-01-14 16:55:34 -0800

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-//import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <h1>WELCOME to web</h1>
+  return <h1>WELCOME to web</h1>;
 }
 
 export default App
