9c021e632c153b76086316a9dcd55a2da5e28a42
sidrayu <sidra@udel.edu>
2025-01-16 10:26:37 -0800

    a1 complet

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 export default function TOC() {
     return (
+      
       <ul>
         <li><Link to="/Labs">Labs</Link></li>
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
