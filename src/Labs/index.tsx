b9d06933334959154ffd1a0dceb4b345248378d9
sidrayu <sidra@udel.edu>
2025-02-04 18:23:40 -0800

    update2

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -10,6 +10,8 @@ export default function Labs() {
       <h2>Chang Yu</h2>
       <h3>Section 01</h3>
       <a href="https://github.com/sidrayu/kambaz-react-web-app-cs5610-sp25/tree/a1">GitHub For Lab 1</a>
+      <a href="https://github.com/sidrayu/kambaz-react-web-app-cs5610-sp25/tree/a2">GitHub For Lab 2</a>
+      
       <br />
       <h1>Labs</h1>
       <TOC />
