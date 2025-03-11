cecaaa0b5803ebadc13dd7a725aa14f4673948a6
sidrayu <sidra@udel.edu>
2025-03-10 18:38:32 -0700

    modified files

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
 import { Button, Card, FormControl } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import * as db from "./Database";
+
 
 export default function Dashboard(
     { courses, course, setCourse, addNewCourse,
         deleteCourse, updateCourse }: {
-        courses: any[]; course: any; setCourse: (course: any) => void;
-        addNewCourse: () => void; deleteCourse: (courseId: string) => void;
-        updateCourse: () => void;
-    }) {
+            courses: any[]; course: any; setCourse: (course: any) => void;
+            addNewCourse: () => void; deleteCourse: (courseId: string) => void;
+            updateCourse: () => void;
+        }) {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const { enrollments } = db;
+
     return (
         <div className="p-4" id="wd-dashboard">
             <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
@@ -62,42 +68,48 @@ export default function Dashboard(
             <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
             <div className="row" id="wd-dashboard-courses">
                 <div className="row row-cols-1 row-cols-md-5 g-4">
-                    {courses.map((course) => (
-                        <div key={course._id} className="col" style={{ width: "300px" }}>
-                            <Card>
-                                <Card.Img src={course.image || "/images/reactjs.jpg"}
-                                    variant="top"
-                                    width="100%"
-                                    height={160} />
-                                <Card.Body>
-                                    <Card.Title className="text-nowrap overflow-hidden">
-                                        {course.name}
-                                    </Card.Title>
-                                    <Card.Text className="overflow-hidden"
-                                        style={{ height: "100px" }}>
-                                        {course.description}
-                                    </Card.Text>
-                                    <Link to={`/Kambaz/Courses/${course._id}/Home`}
-                                        className="btn btn-primary me-2">
-                                        Go
-                                    </Link>
-                                    <Button
-                                        variant="warning"
-                                        id="wd-edit-course-click"
-                                        className="me-2"
-                                        onClick={() => setCourse(course)}>
-                                        Edit
-                                    </Button>
-                                    <Button
-                                        variant="danger"
-                                        id="wd-delete-course-click"
-                                        onClick={() => deleteCourse(course._id)}>
-                                        Delete
-                                    </Button>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                    ))}
+                    {courses.filter((course) =>
+                        enrollments.some(
+                            (enrollment) =>
+                                enrollment.user === currentUser._id &&
+                                enrollment.course === course._id
+                        ))
+                        .map((course) => (
+                            <div key={course._id} className="col" style={{ width: "300px" }}>
+                                <Card>
+                                    <Card.Img src={course.image || "/images/reactjs.jpg"}
+                                        variant="top"
+                                        width="100%"
+                                        height={160} />
+                                    <Card.Body>
+                                        <Card.Title className="text-nowrap overflow-hidden">
+                                            {course.name}
+                                        </Card.Title>
+                                        <Card.Text className="overflow-hidden"
+                                            style={{ height: "100px" }}>
+                                            {course.description}
+                                        </Card.Text>
+                                        <Link to={`/Kambaz/Courses/${course._id}/Home`}
+                                            className="btn btn-primary me-2">
+                                            Go
+                                        </Link>
+                                        <Button
+                                            variant="warning"
+                                            id="wd-edit-course-click"
+                                            className="me-2"
+                                            onClick={() => setCourse(course)}>
+                                            Edit
+                                        </Button>
+                                        <Button
+                                            variant="danger"
+                                            id="wd-delete-course-click"
+                                            onClick={() => deleteCourse(course._id)}>
+                                            Delete
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        ))}
                 </div>
             </div>
         </div>
--- src/Labs/Lab3/ImpliedReturn.tsx
+++ src/Labs/Lab3/ImpliedReturn.tsx
@@ -1,6 +1,6 @@
 const multiply = (a: number, b: number) => a * b;
 const fourTimesFive = multiply(4, 5);
-console.log(fourTimesFive);
+// console.log(fourTimesFive);
 
 export default function ImpliedReturn() {
 return (
