a18415a2aa937b11e1fb7158c36b3ca87d04ed55
sidrayu <sidra@udel.edu>
2025-03-21 23:23:03 -0700

    add labs

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -16,6 +16,8 @@ export default function TOC() {
         active={pathname.includes("Lab3")}> Lab 3 </Nav.Link> </Nav.Item>
       <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab4" id="wd-a4"
         active={pathname.includes("Lab4")}> Lab 4 </Nav.Link> </Nav.Item>
+        <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab5" id="wd-a5"
+        active={pathname.includes("Lab5")}> Lab 5 </Nav.Link> </Nav.Item>
       <Nav.Item> <Nav.Link as={Link} to="/Kambaz" id="wd-a"> Kambaz </Nav.Link> </Nav.Item>
       <Nav.Item> <Nav.Link href="https://github.com/sidrayu" target="_blank"> My GitHub </Nav.Link> </Nav.Item>
     </Nav >
